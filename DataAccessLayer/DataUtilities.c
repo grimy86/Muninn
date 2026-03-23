400df34b985fb895148b3115b42f65b4bb11a52f
grimy86 <grimy86@proton.me>
2026-03-23 22:33:03 +0100

    Entire API compiles in C now

--- DataAccessLayer/DataUtilities.c
+++ DataAccessLayer/DataUtilities.c
@@ -1,22 +1,32 @@
 #include "DataUtilities.h"
 
-bool MDAL_IsValidProcessId(const DWORD processId)
+bool DAL_IsValidProcessId(const DWORD processId)
 {
 	return processId % 4ul == 0ul;
 }
 
-bool MDAL_IsValidHandle(const HANDLE handle)
+bool DAL_IsValidHandle(const HANDLE handle)
 {
 	return (handle != NULL &&
 		handle != INVALID_HANDLE_VALUE);
 }
 
-bool MDAL_IsValidAddress(const DWORD address)
+bool DAL_IsValidAddress(const DWORD address)
 {
 	return address != ERROR_INVALID_ADDRESS;
 }
 
-bool MDAL_IsValidLuid(const LUID luid)
+bool DAL_IsValidLuid(const LUID luid)
 {
 	return (luid.HighPart != 0ul && luid.LowPart != 0ul);
+}
+
+size_t DAL_MinSizeT(const size_t a, const size_t b)
+{
+	return a < b ? a : b;
+}
+
+uint32_t DAL_MinU32(const uint32_t a, const uint32_t b)
+{
+	return (a < b) ? a : b;
 }
--- DataAccessLayer/WindowsProvider32.h
+++ DataAccessLayer/WindowsProvider32.h
@@ -1,8 +1,8 @@
-#ifndef MUNINN_DATA_WINDOWS_PROVIDER_32
-#define MUNINN_DATA_WINDOWS_PROVIDER_32
+#ifndef WINDOWS_PROVIDER_32_H
+#define WINDOWS_PROVIDER_32_H
 
 #include "ApiConfig.h"
-#include "../phnt/phnt_windows.h"
+#include <phnt_windows.h>
 #include <TlHelp32.h>
 #include <ProcessSnapshot.h>
 
@@ -11,59 +11,59 @@
 #endif // !NTSTATUS
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_OpenProcessHandle32(
+DAL_OpenProcessHandle32(
 	_In_ const DWORD processId,
 	_In_ const ACCESS_MASK accessMask,
 	_Out_ HANDLE* const pHandle);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_CloseHandle32(_In_ const HANDLE handle);
+DAL_CloseHandle32(_In_ const HANDLE handle);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_OpenTokenHandle32(
+DAL_OpenTokenHandle32(
 	_In_ const HANDLE processHandle,
 	_In_ const ACCESS_MASK accessMask,
 	_Out_ HANDLE* const pTokenHandle);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_SetSeDebugPrivilege32();
+DAL_SetSeDebugPrivilege32();
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_SetRemoteSeDebugPrivilege32(
+DAL_SetRemoteSeDebugPrivilege32(
 	_In_ const HANDLE tokenHandle);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_SetThreadPriority32(_In_ const DWORD priorityClass);
+DAL_SetThreadPriority32(_In_ const DWORD priorityClass);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_SetThreadSuspended32(_In_ const DWORD threadId);
+DAL_SetThreadSuspended32(_In_ const DWORD threadId);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_SetThreadResumed32(_In_ const DWORD threadId);
+DAL_SetThreadResumed32(_In_ const DWORD threadId);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetThreadPriority32(
+DAL_GetThreadPriority32(
 	_In_ const HANDLE threadHandle,
 	_Out_ INT* const pThreadPriority);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetTokenInfoBufferSize32(
+DAL_GetTokenInfoBufferSize32(
 	_In_ const HANDLE tokenHandle,
-	_In_ const _TOKEN_INFORMATION_CLASS infoClass,
+	_In_ const TOKEN_INFORMATION_CLASS infoClass,
 	_Out_ DWORD* const pRequiredSize);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetSeDebugPrivilege32(
+DAL_GetSeDebugPrivilege32(
 	_In_ const HANDLE tokenHandle,
 	_Out_ BOOL* const pIsSeDebugPrivilegeEnabled);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetProcessInformation32(
+DAL_GetProcessInformation32(
 	_In_ const DWORD processId,
 	_Out_ PROCESSENTRY32W* const pProcessEntry);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetImageFileName32(
+DAL_GetImageFileName32(
 	_In_ const HANDLE processHandle,
 	_Out_writes_(bufferLength)
 	WCHAR* const pBuffer,
@@ -71,25 +71,25 @@ MDAL_GetImageFileName32(
 	_Out_ DWORD* const pCopiedLength);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetModuleBaseAddress32(
+DAL_GetModuleBaseAddress32(
 	_In_ const DWORD processId,
 	_In_ const wchar_t* const pModuleName,
 	_Out_ uintptr_t* const pModuleBaseAddress);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetWindowVisibility32(
+DAL_GetWindowVisibility32(
 	_In_ const DWORD processId,
 	_Out_ BOOL* const pIsWindowVisible);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetProcessArchitecture32(
+DAL_GetProcessArchitecture32(
 	_In_ const HANDLE processHandle,
 	_Out_ USHORT* const pProcessMachine,
 	_Out_ USHORT* const pNativeMachine,
 	_Out_ BOOL* const pIsWow64);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetProcessModules32(
+DAL_GetProcessModules32(
 	_In_ const HANDLE processHandle,
 	_In_ const DWORD processId,
 	_Out_writes_(bufferLength)
@@ -98,7 +98,7 @@ MDAL_GetProcessModules32(
 	_Out_ DWORD* const pCopiedLength);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetProcessThreads32(
+DAL_GetProcessThreads32(
 	_In_ const HANDLE processHandle,
 	_In_ const DWORD processId,
 	_Out_writes_(bufferLength)
@@ -107,7 +107,7 @@ MDAL_GetProcessThreads32(
 	_Out_ DWORD* const pCopiedLength);
 
 MUNINN_API NTSTATUS MUNINN_CALL
-MDAL_GetProcessHandles32(
+DAL_GetProcessHandles32(
 	_In_ const HANDLE processHandle,
 	_In_ const DWORD processId,
 	_Out_writes_(bufferLength)
